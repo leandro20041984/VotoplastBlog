d11e5c6c49f96bad17684dca70ffa0ac82f183b6
leandro20041984 <leandro20041984@gmail.com>
2023-11-28 15:06:54 -0300

    Add project files.

---
+++ VotoplastBlog/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿namespace VotoplastBlog.Models
+{
+    public class Post
+    {
+        public int IdyProperty { get; set; }
+        public string? Title { get; set; }
+        public string? ShortDescription { get; set; }
+
+        //Relation
+        public string? ApplicationUserId { get; set; }
+        public ApplicationUser? ApplicationUser { get; set; }
+        public DateTime? CreatedDate { get; set;} = DateTime.Now;
+        public  string? Description { get; set; }
+        public string? Slug { get; set; }
+    }
+}
